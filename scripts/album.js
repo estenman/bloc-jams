b0b5aa8343c21a5ddb1c8535ab23921bd0855542
estenman <elstenman@gmail.com>
2017-04-03 18:32:58 -0700

    Checkpoint 13 Play Pause: completed

--- scripts/album.js
+++ scripts/album.js
@@ -115,13 +115,14 @@ var currentlyPlayingSong = null;
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
+    
     songListContainer.addEventListener('mouseover', function(event) {
-        var songItem = getSongItem(event.target);
-        var songItemNumber = songItem.getAttribute('data-song-number');
-        if (songItem === 'album-view-song-item' && songItemNumber !== currentlyPlayingSong) {
-            songItem.innerHTML = playButtonTemplate;
+        if (event.target.parentElement.className === 'album-view-song-item') {
+            var songItem = getSongItem(event.target);
+            if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+                songItem.innerHTML = playButtonTemplate;
+            }
         }
-        
     });
     
     for (var i = 0; i < songRows.length; i++) {
