1f5a415967614add42df796e1e6acc3e147a14f3
estenman <elstenman@gmail.com>
2017-04-03 19:36:45 -0700

    Assignment 13 Play Pause: complete

--- scripts/album.js
+++ scripts/album.js
@@ -60,14 +60,18 @@ var setCurrentAlbum = function(album) {
 };
 
 var findParentByClassName = function(element, targetClass) {
-    if (element) {
         var currentParent = element.parentElement;
-        while (currentParent.className !== targetClass && currentParent.className !== null) {
-        currentParent = currentParent.parentElement;
-    }
-    return currentParent;
-    }
-};
+        if (currentParent === null) {
+    	console.log("No parent found");
+        } else if (currentParent !== null) {
+            while (currentParent.className !== targetClass && currentParent.className !== null) {
+            currentParent = currentParent.parentElement;
+        } 
+        return currentParent;
+        } else {
+        console.log("No parent found with that class name")
+        }
+    };
 
 var getSongItem = function (element) { 
     switch(element.className) {
